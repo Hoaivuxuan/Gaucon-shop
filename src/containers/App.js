8926f780bbe7062c38cc8e22eac44fd6a3b644cc
Hoai <vuxuanhoai28@gmail.com>
2023-03-09 10:42:55 +0700

    updated

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,19 +4,15 @@ import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter as Router } from 'connected-react-router';
 import { history } from '../redux'
 import { ToastContainer } from 'react-toastify';
-
-
+//
 import { userIsAuthenticated, userIsNotAuthenticated } from '../hoc/authentication';
-
 import { path } from '../utils'
-
+import { CustomToastCloseButton } from '../components/CustomToast';
+//
 import Home from '../routes/Home';
-// import Login from '../routes/Login';
 import Login from './Auth/Login';
 import Header from './Header/Header';
 import System from '../routes/System';
-
-import { CustomToastCloseButton } from '../components/CustomToast';
 import ConfirmModal from '../components/ConfirmModal';
 
 class App extends Component {
